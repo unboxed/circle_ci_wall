171f2959aac0fd1d54db0d22814c08ab3e79ba32
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-06-25 09:51:21 +0200

    remove test exception from airbrake middleware

--- middleware/airbrake.rb
+++ middleware/airbrake.rb
@@ -7,7 +7,6 @@ module Goliath
       def call(env, *args)
         response = super(env, *args)
         if env[RACK_EXCEPTION]
-          ::Airbrake.notify(Exception.new("test!!"), :env => ENV)
           ::Airbrake.notify(env[RACK_EXCEPTION], :env => ENV)
         end
         response
